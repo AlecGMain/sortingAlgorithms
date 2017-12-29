a826b94940af8fbfb0b5eedd2e4f012d543f886e
AlecGMain <geosimonianalec@gmail.com>
2017-12-28 17:26:05 -0800

    Finished branch on BubbleSort, but project is NOT complete yet.

--- AlecSortingAlgorithims/AlecSortingAlgorithims/Program.cs
+++ AlecSortingAlgorithims/AlecSortingAlgorithims/Program.cs
@@ -10,6 +10,56 @@ namespace AlecSortingAlgorithims
     {
         static void Main(string[] args)
         {
+            //Testing algorithim.
+            int[] array = new int[100];
+            Random r = new Random();
+
+            for ( int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(0, 100);
+            }
+            for(int i = 0; i<array.Length; i++)
+            {
+                Console.Write("{0}, ", array[i]);
+            }
+            Console.WriteLine();
+
+            array = BubbleSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}, ", array[i]);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
+        static int[] BubbleSort(int[] array)
+        {
+            int allsort = 0;
+            while (allsort != array.Length - 1)
+            {
+                allsort = 0;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+
+                    if (array[i] > array[i + 1])
+                    {
+                        int second = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = second;
+
+                    }
+                    else
+                    {
+                        allsort++;
+                    }
+
+                }
+                
+            }
+            return array;
+        }
+
     }
 }
